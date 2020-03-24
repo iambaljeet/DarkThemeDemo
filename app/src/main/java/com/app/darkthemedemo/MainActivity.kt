c860ddd3f0355721ceb3485636310fd2d045adab
Baljeet <ibaljeet@outlook.com>
2020-03-24 15:39:35 +0530

    Initial commit

---
+++ app/src/main/java/com/app/darkthemedemo/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.app.darkthemedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        switch_theme.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+    }
+}
